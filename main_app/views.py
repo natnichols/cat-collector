9d23c19c1bef4d8afb1c961ab541e1e8ae1c62e6
NatNichols <natalie.e.nichols@gmail.com>
2024-02-26 15:49:08 -0500

    add cat_index functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,28 @@
+# import
 from django.shortcuts import render
-
-# Add the following import
 from django.http import HttpResponse
 
-# Define the home view
+# temp data / models
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+  Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+# Create views here
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
-def about (request):
+def about(request):
   return render(request, 'about.html')
+
+def cat_index(request):
+  return render(request, 'cats/index.html', {'cats': cats})
