d04b4533db5ef0348d7b9b478e6f38f6818fbcdb
NatNichols <natalie.e.nichols@gmail.com>
2024-02-27 11:22:22 -0500

    remove test Cat data

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,6 @@
 # import
 from django.shortcuts import render
 
-# temp data / models
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-  Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
 # Create views here
 def home(request):
   return render(request, 'home.html')
