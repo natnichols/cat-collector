d4ca4ce65b33cdccdecc48804126d29edbbbdc67
NatNichols <natalie.e.nichols@gmail.com>
2024-02-28 13:36:37 -0500

    add class Meta with ordering Model Meta option to Feeding Model

--- main_app/models.py
+++ main_app/models.py
@@ -30,5 +30,7 @@ class Feeding(models.Model):
   cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
   def __str__(self):
-      return f"{self.get_meal_display()} on {self.date}"
+    return f"{self.get_meal_display()} on {self.date}"
   
+  class Meta:
+    ordering = ['-date']
