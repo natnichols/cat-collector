2bacdc09d855f8cfe7f187bd6325e1bf912cf53a
NatNichols <natalie.e.nichols@gmail.com>
2024-02-28 14:09:17 -0500

    create fed_for_today custom model method to Cat Model class

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -19,6 +20,9 @@ class Cat(models.Model):
   
   def get_absolute_url(self):
     return reverse('cat-detail', kwargs={'cat_id': self.id})
+  
+  def fed_for_today(self):
+    return self.feeding_set.filter(data=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
