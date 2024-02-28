e2fc7fe8db71032885fab5bfcd61d79a530fbfde
NatNichols <natalie.e.nichols@gmail.com>
2024-02-28 11:02:31 -0500

    import FeedingForm into views, refactor cat_detail fn to render FeedingForm

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
 
 # Create views here
 def home(request):
@@ -16,7 +17,11 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', { 'cat': cat })
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {
+    'cat': cat,
+    'feeding_form': feeding_form
+  })
 
 class CatCreate(CreateView):
   model = Cat
