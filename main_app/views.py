3937b937aa6ed8d0cc7dfa821bf92f537f3cc97c
NatNichols <natalie.e.nichols@gmail.com>
2024-02-29 15:26:36 -0500

    refactor CatCreate class to not include Toys field

--- main_app/views.py
+++ main_app/views.py
@@ -37,9 +37,7 @@ def add_feeding(request, cat_id):
 
 class CatCreate(CreateView):
   model = Cat
-  fields = '__all__'
-  # fields = ['name', 'breed', 'description', 'age']
-  # success_url = '/cats/'
+  fields = ['name', 'breed', 'description', 'age']
 
 class CatUpdate(UpdateView):
   model = Cat
