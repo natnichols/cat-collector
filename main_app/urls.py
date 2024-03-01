6b137a97d80a498a58362d4b3b524c7ee529db9e
NatNichols <natalie.e.nichols@gmail.com>
2024-03-01 10:30:20 -0500

    implement signup functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
   path('toys/', views.ToyList.as_view(), name='toy-index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete'),
+  path('accounts/signup/', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,12 +3,28 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Cat, Toy
 from .forms import FeedingForm
 
 # Create views here
 # def home(request):
 #   return render(request, 'home.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
 
 def about(request):
   return render(request, 'about.html')
