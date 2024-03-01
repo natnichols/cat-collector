05df79c91f86e336561f163ae3db3a746ad2e0be
NatNichols <natalie.e.nichols@gmail.com>
2024-03-01 10:34:01 -0500

    refactor cat_index view fn to filter and only render the logged-in user's cats

--- main_app/views.py
+++ main_app/views.py
@@ -30,7 +30,7 @@ def about(request):
   return render(request, 'about.html')
 
 def cat_index(request):
-  cats = Cat.objects.all()
+  cats = Cat.objects.filter(user=request.user)
   return render(request, 'cats/index.html', {'cats': cats})
 
 def cat_detail(request, cat_id):
